681fd435f186feaaee092ea7ed680d7a677fd0af
Aju506 <160851385+Aju506@users.noreply.github.com>
2024-03-26 11:06:58 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,94 @@
+# print('hello')
+# print(4567)
+# print(type("hello"))
+# x='hello'
+# print(type(x))
+# x=24j
+# print(type(x))
+# x=float(5)
+# print(x+10)
+# print(type(x))
+# print(x)
+# x=str(6)
+# print(type(x))
+# print(x+'9')
+# x=5
+# y="python"
+# print(x)
+# print(y)
+# x=4
+# x="python"
+# print(x)
+# x="hello"
+# print(type(x))
+# x=(35)
+# print(type(x))
+# x=(23.4)
+# print(type(x))
+# x=str(6)
+# print(type(x))
+# x="hello world"
+# print(x)
+# print(type(x))
+# x=(23)
+# print(type(x))
+# word=' python coding'
+# print(word)
+# print(type(word))
+# print(len(word))
+# print(word[-1])
+# name=' python tutorial '
+# print(name)
+# print(type(name))
+# print(len(name))
+# print(name[0])
+# print(name[-1])
+# print(name[5])
+# print(name[-4])
+# print(word[2:5])
+# print(word[-7:-2])
+# print(word[3:])
+# print(word[:6])
+
+# print(word[:])
+# print(word[1:12:1])
+# print(word[1:12:3])
+
+# print(word.upper())
+# print(word.lower())
+# print(word.capitalize())
+# print(word[1:12:2])
+# print(word.strip())
+
+# x=' pyt hon'
+# print(x)
+# print(x.strip())
+
+# x='hello'
+# print(x.replace('h','j'))
+
+# a = "Hello, Wor,ld!"
+# print(a.split(","))
+
+x='hello'
+y='world'
+print(x+y)
+print(x+" "+y)
+print(x,y)
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+print(3**2)
+x=5
+# x=x+1
+x+=1
+print(x)
+s
